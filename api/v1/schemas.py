884643344177a11105d01ee004bd13431e6fdd53
Paolo <paolo.herrera.araya@gmail.com>
2025-08-13 23:23:56 -0400

    Add language, brochure_type and company name in request

--- api/v1/schemas.py
+++ api/v1/schemas.py
@@ -2,4 +2,7 @@ from pydantic import BaseModel, HttpUrl
 
 class ScrapeRequest(BaseModel):
     url: HttpUrl
+    language: str = "English"
+    brochure_type: str = "professional"
+    company_name: str = None
     
